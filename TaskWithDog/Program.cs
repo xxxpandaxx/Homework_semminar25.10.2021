03401b02e4e28f23a0e700e25f595638cf767ecb
xxxpandaxx <aman77345@gmail.com>
2021-10-27 17:37:44 +0300

    homework

---
+++ TaskWithDog/Program.cs
@@ -0,0 +1,30 @@
+﻿double distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int friend = 2;
+int count = 0;
+double time = 0;
+
+
+while(distance > 10)
+{
+    if  (friend == 1)
+    {
+         time = distance / (SecondFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+    else
+    {
+         time = distance / (FirstFriendSpeed + DogSpeed);
+        friend = 1;
+    }    
+        
+
+distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+count++;
+}
+
+
+Console.Write("Собака пробежит : "+count+" раз!");
+
